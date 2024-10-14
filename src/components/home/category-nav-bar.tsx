295f8cdff4281d7586c4ca852dd6f487142195ee
Max <111196993+NqMax@users.noreply.github.com>
2024-10-13 23:58:25 -0600

    feat: add category navigation bar component

---
+++ src/components/home/category-nav-bar.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+
+export function CategoryNavBar() {
+  return (
+    <nav className="container mx-auto flex h-20 items-center justify-around p-2">
+      <Button>Featured Pieces</Button>
+      <Button>Abstract Art</Button>
+      <Button>Oil Paintings</Button>
+      <Button>Digital Photography</Button>
+      <Button>Wall Sculpture</Button>
+      <Button>Landscapes</Button>
+      <Button>Graffiti Drawings</Button>
+      <Button>Fine Arts Prints</Button>
+      <Button>Installations</Button>
+      <Button>Large Works</Button>
+    </nav>
+  );
+}
